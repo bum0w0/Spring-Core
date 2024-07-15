1ae20b1c480fd0e0bd12acf645e1381ddc48161d
kim-jin-bum <kgy5988@naver.com>
2024-07-16 00:53:21 +0900

    refactor: 주문 서비스 및 할인 정책 관련 리팩토링
    주문 서비스 클라이언트(OrderServiceImpl)가 인터페이스 뿐만 아니라 구현 클래스에도 의존하게 되는 문제(OCP 위반)를 수정
    할인 정책 변경 시 클라이언트(OrderServiceImpl)의 소스코드를 수정하게 되어 DIP에 위반되는 사항 수정
    - 향후 할인 정책 변경 시 OrderServiceImpl의 소스코드 수정이 필요 없도록 개선, AppConfig에서 구현 객체를 생성하고 해당 객체 인스턴스의 참조(레퍼런스)를 생성자를 통해서 주입

--- src/test/java/hello/core/member/MemberServiceTest.java
+++ src/test/java/hello/core/member/MemberServiceTest.java
@@ -1,11 +1,20 @@
 package hello.core.member;
 
+import hello.core.AppConfig;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class MemberServiceTest {
 
-    MemberService memberService = new MemberServiceImpl();
+    MemberService memberService;
+
+    // @BeforeEach 를 사용하여 테스트 메서드가 실행될 때마다 객체를 새로 초기화, 테스트 메서드 간에 독립성이 보장됨
+    @BeforeEach
+    public void beforeEach() {
+        AppConfig appConfig = new AppConfig();
+        memberService = appConfig.memberService();
+    }
 
     @Test
     void join() {
