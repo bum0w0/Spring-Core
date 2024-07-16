9233cf5e6e98e372fd059759d645c3842b544848
kim-jin-bum <kgy5988@naver.com>
2024-07-16 22:27:58 +0900

    refactor: AppConfig 리팩터링
    객체 생성 로직을 개별 메서드로 분리함으로써 재사용 가능하도록 함
    구현체(ex.할인 정책)의 변경이 쉽고, 확장가능하도록 수정

---
+++ src/main/java/hello/core/AppConfig.java
@@ -0,0 +1,29 @@
+package hello.core;
+
+import hello.core.discount.DiscountPolicy;
+import hello.core.discount.FixDiscountPolicy;
+import hello.core.member.MemberRepository;
+import hello.core.member.MemberService;
+import hello.core.member.MemberServiceImpl;
+import hello.core.member.MemoryMemberRepository;
+import hello.core.order.OrderService;
+import hello.core.order.OrderServiceImpl;
+
+public class AppConfig {
+    public MemberService memberService() {
+        return new MemberServiceImpl(memberRepository());
+    }
+    public OrderService orderService() {
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
+    }
+
+    public MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
+
+    // 할인 정책 변경이 용이하도록 수정
+    public DiscountPolicy discountPolicy() {
+        // return new [새로운 할인 정책]
+        return new FixDiscountPolicy();
+    }
+}
