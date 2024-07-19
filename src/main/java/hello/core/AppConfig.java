fc5e37c9fecd85b6a2b5b2e659de5a67231bbf6f
kim-jin-bum <kgy5988@naver.com>
2024-07-19 17:29:15 +0900

    refactor: 스프링 컨테이너를 통해 DI 적용

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -8,19 +8,25 @@ import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+//
+@Configuration
 public class AppConfig {
+    @Bean
     public MemberService memberService() {
         return new MemberServiceImpl(memberRepository());
     }
+    @Bean
     public OrderService orderService() {
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
-
+    @Bean
     public MemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
-
+    @Bean
     // 할인 정책 변경이 용이하도록 수정
     public DiscountPolicy discountPolicy() {
 //        return new FixDiscountPolicy();
--- src/main/java/hello/core/MemberApp.java
+++ src/main/java/hello/core/MemberApp.java
@@ -3,11 +3,19 @@ package hello.core;
 import hello.core.member.Grade;
 import hello.core.member.Member;
 import hello.core.member.MemberService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class MemberApp {
     public static void main(String[] args) {
-        AppConfig appConfig = new AppConfig();
-        MemberService memberService = appConfig.memberService();
+//        AppConfig appConfig = new AppConfig();
+//        MemberService memberService = appConfig.memberService();
+
+        //ApplicationContext를 스프링 컨테이너라고 한다.
+        // 컨테이너에 Bean으로 등록된 객체들을 applicationContext 객체가 다 관리함 (AppConfig 클래스에서 어노테이션으로 빈 등록을 헸으니 아래와 같이 생성)
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        // 관례적으로 객체명은 ac 라고 함, 파라미터 값에 이름은 메소드 이름이 들어감 (이 메소드의 객체를 찾겠다.) 두번째 파라미터는 타입
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
 
         Member member = new Member(1L, "memberA", Grade.VIP);
         memberService.join(member);
