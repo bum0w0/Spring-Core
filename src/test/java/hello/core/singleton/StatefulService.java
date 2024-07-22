04c2ea6f152a1d0c1abf4588ca0e2dc4a5e6012b
kim-jin-bum <kgy5988@naver.com>
2024-07-23 00:31:08 +0900

    test: 싱글톤 방식에서 객체가 무상태를 유지하도록 수정

--- src/test/java/hello/core/singleton/StatefulService.java
+++ src/test/java/hello/core/singleton/StatefulService.java
@@ -7,14 +7,10 @@ public class StatefulService {
       특정 클라이언트가 값을 변경할 수 있는 필드가 있으면 안된다.
       스프링 빈의 필드에 공유 값을 설정하면 큰 장애가 발생할 수 있음.
  */
-    private int price; // 상태를 유지하는 필드
 
-    public void order(String name, int price) {
+    public int order(String name, int price) {
         System.out.println("name = " + name + " price = " + price);
-        this.price = price; // 싱글톤 방식에서 객체는 무상태(stateless)로 설계해야하는데 이렇게 상태(stateful)를 가지면 문제가 생긴다.
-    }
-
-    public int getPrice() {
+        // 싱글톤 방식에서 객체는 무상태(stateless)로 설계
         return price;
     }
 }
--- src/test/java/hello/core/singleton/StatefulServiceTest.java
+++ src/test/java/hello/core/singleton/StatefulServiceTest.java
@@ -5,9 +5,6 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Bean;
 
-
-import static org.assertj.core.api.Assertions.assertThat;
-
 class StatefulServiceTest {
 
     // 싱글톤 방식의 주의점
@@ -17,14 +14,14 @@ class StatefulServiceTest {
         StatefulService statefulService1 = ac.getBean(StatefulService.class);
         StatefulService statefulService2 = ac.getBean(StatefulService.class);
 
-        statefulService1.order("userA", 10000);
-        statefulService2.order("userB", 20000);
+        int userA_price = statefulService1.order("userA", 10000);
+        int userB_price = statefulService2.order("userB", 20000);
 
         // userA의 주문금액 10000원이 아닌 20000원이 반환되는 문제가 발생
-        int userA_price = statefulService1.getPrice();
+//        int userA_price = statefulService1.getPrice();
         System.out.println("userA_price = " + userA_price);
 
-        assertThat(statefulService1.getPrice()).isEqualTo(20000);
+//        assertThat(statefulService1.getPrice()).isEqualTo(20000);
     }
 
     static class TestConfig {
