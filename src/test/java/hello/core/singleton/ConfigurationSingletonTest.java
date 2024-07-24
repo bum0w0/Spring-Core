fee3652797234f061bcd19b172f670864f825873
kim-jin-bum <kgy5988@naver.com>
2024-07-24 17:18:40 +0900

    test: CGLIB의 내부 기술을 통한 스프링 빈의 싱글톤 보장

--- src/test/java/hello/core/singleton/ConfigurationSingletonTest.java
+++ src/test/java/hello/core/singleton/ConfigurationSingletonTest.java
@@ -29,4 +29,17 @@ public class ConfigurationSingletonTest {
         assertThat(memberService.getMemberRepository()).isSameAs(memberRepository);
         assertThat(orderService.getMemberRepository()).isSameAs(memberRepository);
     }
+
+    // 스프링은 어떻게 싱글톤을 보장하는가?
+    @Test
+    void configurationDeep() {
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+        AppConfig bean = ac.getBean(AppConfig.class);
+        System.out.println("bean = " + bean.getClass());
+
+        // bean = class hello.core.AppConfig$$SpringCGLIB$$0
+        //스프링이 CGLIB 이라는 바이트코드 조작 라이브러리를 사용해서 AppConfig 클래스를 상속받은 임의의 다른 클래스(AppConfig@CGLIB)를 만들고, 그 다른 클래스를 스프링 빈으로 등록
+        //CGLIB 기술로 스프링 빈이 존재하면 존재하는 빈을 반환하고, 스프링 빈이 없으면 생성해서 스프링 빈으로 등록하고 반환하는 코드가 동적으로 만들어진다. 따라서 싱글톤이 보장된다.
+        //@Configuration을 사용하지않고 @Bean만 사용해도 스프링 빈으로 등록되지만, 싱글톤을 보장하지 않는다. 그러니 스프링 설정 정보에는 항상 @Configuration을 적용하자
+    }
 }
