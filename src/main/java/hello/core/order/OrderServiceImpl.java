e1eae8b7c712593d6987dad9914f2029edc79f83
kim-jin-bum <kgy5988@naver.com>
2024-07-28 17:58:12 +0900

    feat: 컴파일시 타입 체크를 위해 애노테이션 직접 생성
    조회하는 빈이 2개 이상일 때 @Qualifier에 문자를 적으면 컴파일시 타입 체크가 안되기 때문에 애노테이션을 직접 생성

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -1,13 +1,12 @@
 package hello.core.order;
 
+import hello.core.annotation.MainDiscountPolicy;
 import hello.core.discount.DiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
-import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
 @Component
-@RequiredArgsConstructor
 public class OrderServiceImpl implements OrderService {
 
     private final MemberRepository memberRepository;
@@ -15,11 +14,10 @@ public class OrderServiceImpl implements OrderService {
 
     // 스프링 컨테이너가 빈(Bean)으로 등록된 객체들 중에서, 해당 타입에 맞는 빈을 찾아 주입 (MemoryMemberRepository, RateDiscountPolicy)
     // `@Autowired` 를 사용하면 생성자에서 여러 의존관계도 한번에 주입받을 수 있다.
-
-    /*
-        기존의 생성자 코드를 롬복 라이브러리가 제공하는 `@RequiredArgsConstructor` 기능을 사용하여 final이 붙은 필드를 모아서 생성자를 자동으로 생성 하는것으로 대체
-    */
-
+    public OrderServiceImpl(MemberRepository memberRepository, @MainDiscountPolicy DiscountPolicy discountPolicy) {
+        this.memberRepository = memberRepository;
+        this.discountPolicy = discountPolicy;
+    }
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
